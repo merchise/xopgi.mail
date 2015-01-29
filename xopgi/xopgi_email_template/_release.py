578b85f5a90def4fd560bdcbd7f27016db2311eb
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-01-29 11:24:36 -0500

    Moves email_template from it's repo.

---
+++ xopgi/xopgi_email_template/_release.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------
+# xopgi.email_template._release
+#----------------------------------------------------------------------
+# Copyright (c) 2013, 2014 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2014-04-24
+
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        unicode_literals as _py3_unicode,
+                        absolute_import as _py3_abs_imports)
+
+
+def read_terpfile():
+    import os
+    from os.path import join
+    with open(join(os.path.dirname(__file__), '__openerp__.py'), 'rU') as fh:
+        content = fh.read()
+        return eval(content, {}, {})
+
+_TERP = read_terpfile()
+VERSION = _TERP['version']
