dae14196b7003ca07d423f75f8104eb1a9ba8869
Manuel Vázquez Acosta <manu@merchise.org>
2015-03-17 08:12:47 -0400

    Don't stop sending emails after the first id.

--- xopgi/xopgi_mail_verp/mail_mail.py
+++ xopgi/xopgi_mail_verp/mail_mail.py
@@ -17,21 +17,22 @@ Override the send method to add mail_id on context.
 
 from openerp.osv import osv
 
+
 class mail_mail(osv.Model):
     _name = 'mail.mail'
     _inherit = _name
 
     def send(self, cr, uid, ids, **kwargs):
-        '''
-        Add mail_id to context and call _super one by one.
+        '''Add mail_id to context and call _super one by one.
 
         mail_id may use to get unique return-path.
 
         '''
+        res = True
         context = kwargs.get('context', {})
         _super = super(mail_mail, self).send
         for _id in ids:
             context.update(mail_id=_id)
             kwargs.update(context=context)
-            return _super(cr, uid, [_id], **kwargs)
-        return True
+            res = _super(cr, uid, [_id], **kwargs) and res
+        return res
