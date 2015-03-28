5296e6f83cbd245d84afb492c37799129be24ada
Manuel Vázquez Acosta <manu@merchise.org>
2015-03-27 22:37:47 -0400

    Fix VERP addresses encoding.
    The previous encoding was fooled by giving the same encoding to both
    "@=" and "=@".  This encoding is a bit more complicated but also more
    reliable.

--- xopgi/xopgi_mail_verp/verpcoder.py
+++ xopgi/xopgi_mail_verp/verpcoder.py
@@ -20,6 +20,9 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
+ESCAPED = {'@': '=.', '=': '=='}
+UNESCAPED = {val: key for key, val in ESCAPED.items()}
+
 
 def encode(email):
     '''Encodes an email so that is could be embedded in another address.
@@ -27,15 +30,28 @@ def encode(email):
     Examples::
 
        >>> encode('some@address.com')
-       'some=address.com'
+       'some=.address.com'
 
-       >>> encode('some==thing=@else.com=')
-       'some====thing===else.com=='
+       >>> encode('some=.thing@=else.com=')
+       'some==.thing=.==else.com=='
 
     Decoding this should be done with `decode`:func:.
 
     '''
-    return email.replace('=', '==').replace('@', '=')
+    # Escaping: "@" becomes "=."; "=" becomes "=="; a single "." does not
+    # needs to be escaped.  If the email contains "=." it will turn into "==."
+    # the second "=" won't be regarded as an escape char.
+    from six import integer_types as position
+    res = ''
+    while email:
+        pos = min(find(email, ch) for ch in ESCAPED.keys())
+        if isinstance(pos, position):
+            res += email[:pos] + ESCAPED[email[pos]]
+            email = email[pos+1:]
+        else:
+            res += email
+            email = ''
+    return res
 
 
 def decode(vmail):
@@ -49,7 +65,41 @@ def decode(vmail):
        >>> decode(encode('some==thing=@else.com='))
        'some==thing=@else.com='
 
-    Decoding this should be done with `decode`:func:.
+       >>> decode(encode('some=.@=else.com'))
+       'some=.@=else.com'
 
     '''
-    return vmail.replace('=', '@').replace('@@', '=')
+    from six import integer_types as position
+    res = ''
+    while vmail:
+        pos = min(find(vmail, ch) for ch in UNESCAPED.keys())
+        if isinstance(pos, position):
+            res += vmail[:pos] + UNESCAPED[vmail[pos:pos+2]]
+            vmail = vmail[pos+2:]
+        else:
+            res += vmail
+            vmail = ''
+    return res
+
+
+# Helper to allow min(find()...) in our main algorithm to work.  This is
+# basically a position which is always bigger than any other position, but
+# that is not an integer.
+class NotFoundType(object):
+    def __lt__(self, x):
+        return False
+    def __gt__(self, x):
+        return True
+    def __bool__(self, x):
+        return False
+    __nonzero__ = __bool__
+
+NotFound = NotFoundType()
+
+
+def find(s, what):
+    res = s.find(what)
+    if res < 0:
+        return NotFound
+    else:
+        return res
