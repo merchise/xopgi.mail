6174c65fd2ffbfdaf8179340fc806b137b640e6d
Manuel Vázquez Acosta <manu@merchise.org>
2015-05-11 18:11:11 -0400

    Improve alias selection docstrings.

--- xopgi/xopgi_mail_verp/common.py
+++ xopgi/xopgi_mail_verp/common.py
@@ -19,10 +19,27 @@ from __future__ import (division as _py3_division,
 from openerp.release import version_info as ODOO_VERSION_INFO
 
 
+# The name of the configuration parameter where the alias for bounces should
+# be located.
+MAIL_BOUNCE_ALIAS_PARAM = 'mail.bounce.alias'
+
+# The default alias.
+DEFAULT_BOUNCE_ALIAS = 'postmaster-odoo'
+
+
 def get_bounce_alias(pool, cr, uid, context=None):
+    '''Return the alias for building bouncing addresses.
+
+    Will try to the alias from the MAIL_BOUNCE_ALIAS_PARAM, if not defined we
+    fall back to the 'mail.catchall.alias' (only in OpenERP).
+
+    If no alias is defined, return 'postmaster-odoo'.
+
+    '''
     alias_name = ('mail.catchall.alias'
                   if ODOO_VERSION_INFO < (8, 0)
-                  else 'mail.bounce.alias')
+                  else MAIL_BOUNCE_ALIAS_PARAM)
     get_param = pool['ir.config_parameter'].get_param
-    return get_param(cr, uid, alias_name,
-                     default='postmaster-odoo', context=context)
+    res = get_param(cr, uid, alias_name,
+                    default=DEFAULT_BOUNCE_ALIAS, context=context)
+    return res
