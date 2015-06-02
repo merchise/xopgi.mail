946f5f88d02adf49cccbe49ba97dc7f57b0d5aa1
Manuel Vázquez Acosta <manu@merchise.org>
2015-06-02 17:17:31 -0400

    Separate VERP alias from normal mail.mail bounces aliases.

--- xopgi/xopgi_mail_verp/common.py
+++ xopgi/xopgi_mail_verp/common.py
@@ -23,6 +23,11 @@ from openerp.release import version_info as ODOO_VERSION_INFO
 # be located.
 MAIL_BOUNCE_ALIAS_PARAM = 'mail.bounce.alias'
 
+# The name of the preferred alias for VERP bounces.  If this alias is set the
+# 'mail.bounces.alias' won't be used.  This allows to bypass the Odoo
+# mail.mail default Return-Path, though VERP is not installed.
+MAIL_BOUNCE_VERP_ALIAS_PARAM = 'mail.bounce.verp.alias'
+
 # The default alias.
 DEFAULT_BOUNCE_ALIAS = 'postmaster-odoo'
 
@@ -30,16 +35,15 @@ DEFAULT_BOUNCE_ALIAS = 'postmaster-odoo'
 def get_bounce_alias(pool, cr, uid, context=None):
     '''Return the alias for building bouncing addresses.
 
-    Will try to the alias from the MAIL_BOUNCE_ALIAS_PARAM, if not defined we
-    fall back to the 'mail.catchall.alias' (only in OpenERP).
-
-    If no alias is defined, return 'postmaster-odoo'.
+    If no alias are set, return 'postmaster-odoo'.
 
     '''
     alias_name = ('mail.catchall.alias'
                   if ODOO_VERSION_INFO < (8, 0)
                   else MAIL_BOUNCE_ALIAS_PARAM)
     get_param = pool['ir.config_parameter'].get_param
-    res = get_param(cr, uid, alias_name,
-                    default=DEFAULT_BOUNCE_ALIAS, context=context)
+    bounce = get_param(cr, uid, alias_name,
+                       default=DEFAULT_BOUNCE_ALIAS, context=context)
+    res = get_param(cr, uid, MAIL_BOUNCE_VERP_ALIAS_PARAM, default=bounce,
+                    context=context)
     return res
