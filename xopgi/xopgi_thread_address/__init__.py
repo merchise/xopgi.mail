96392c3776a8659f666b08e8cdb11aef5081957b
Manuel Vázquez Acosta <manu@merchise.org>
2015-06-11 16:51:46 -0400

    Add the addon to generate a unique address per thread.

---
+++ xopgi/xopgi_thread_address/__init__.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# xopgi_thread_address
+# ---------------------------------------------------------------------
+# Copyright (c) 2015 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2015-06-03
+
+'''Implement a router/transport that ensures threads have a unique Reply-To
+address.
+
+'''
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+
+from openerp import SUPERUSER_ID
+from openerp.addons.xopgi_mail_threads import MailRouter, MailTransportRouter
+from openerp.addons.xopgi_mail_threads import TransportRouteData
+
+
+# The default prefix for a Reply-To address.
+DEFAULT_REPLYTO_PREFIX = 'replyto'
+
+
+class UniqueAddressTransport(MailTransportRouter):
+    @classmethod
+    def query(cls, obj, cr, uid, message, context=None):
+        msg, _ = cls.get_message_objects(obj, cr, uid, message,
+                                         context=context)
+        if msg and msg.thread_index:
+            address = cls._get_replyto_address(obj, cr, uid, msg.thread_index,
+                                               context=context)
+            return bool(address), dict(thread_index=msg.thread_index,
+                                       replyto_address=address)
+        return False, None
+
+    def prepare_message(self, obj, cr, uid, message, data=None, context=None):
+        thread_index = data['thread_index']
+        address = data['replyto_address']
+        self._set_replyto_address(obj, cr, uid, message, address,
+                                  context=context)
+        # The 'Thread-Index' is just to fuck some Microsoft Offices out there
+        # that are not including the References.
+        message['X-Odoo-Thread-Index'] = message['Thread-Index'] = thread_index
+        return TransportRouteData(message, {})
+
+    @classmethod
+    def _set_replyto_address(cls, obj, cr, uid, message, address,
+                             context=None):
+        '''Set the Reply-To address for the message given the thread index.
+
+        Furthermore, all authors and sender are reset so their addresses are
+        the same.
+
+        '''
+        from email.utils import getaddresses, formataddr
+        from openerp.addons.mail.mail_thread import decode_header
+        from openerp.addons.base.ir.ir_mail_server import encode_rfc2822_address_header
+        del message['Reply-To']
+        message['Reply-To'] = address
+        sender = decode_header(message, 'Sender')
+        if sender:
+            sender_name, _ = getaddresses([sender])[0]
+            message.replace_header(
+                'Sender',
+                encode_rfc2822_address_header(formataddr((sender_name, address)))
+            )
+        from_list = []
+        for authorname, _ in getaddresses([decode_header(message, 'From')]):
+            from_list.append(formataddr((authorname, address)))
+        if from_list:
+            del message['From']
+            message['From'] = encode_rfc2822_address_header(', '.join(from_list))
+
+    @classmethod
+    def _get_replyto_address(cls, obj, cr, uid, thread_index,
+                             context=None):
+        get_param = obj.pool['ir.config_parameter'].get_param
+        replyto = get_param(cr, uid, 'mail.replyto.alias',
+                            default=DEFAULT_REPLYTO_PREFIX, context=context)
+        domain = get_param(cr, uid, 'mail.catchall.domain', context=context)
+        if domain:
+            return '%s-%s@%s' % (replyto, thread_index, domain)
+        else:
+            return None
+
+
+class UniqueAddressRouter(MailRouter):
+    @classmethod
+    def query(cls, obj, cr, uid, message, context=None):
+        from email.utils import getaddresses
+        from openerp.addons.mail.mail_thread import decode_header
+        headers = [
+            decode_header(message, 'To'),
+            decode_header(message, 'Cc'),
+            decode_header(message, 'Delivered-To')
+        ]
+        recipients = [email for _, email in getaddresses(headers)]
+        match = cls._find_replyto(obj, cr, uid, recipients, context=context)
+        return match is not None, match
+
+    @classmethod
+    def _find_replyto(cls, obj, cr, uid, recipients, context=None):
+        from xoutil.string import cut_prefix, cut_suffix
+        get_param = obj.pool['ir.config_parameter'].get_param
+        domain = get_param(cr, uid, 'mail.catchall.domain', context=context)
+        if not domain:
+            return None
+        replyto = get_param(cr, uid, 'mail.replyto.alias',
+                            default=DEFAULT_REPLYTO_PREFIX, context=context)
+        prefix, suffix = replyto + '-', '@' + domain
+        found, model, thread_id = None, None, None
+        Threads = obj.pool['mail.thread']
+        while not found and recipients:
+            res = recipients.pop(0)
+            if res.startswith(prefix):
+                thread_index = cut_prefix(cut_suffix(res, suffix), prefix)
+                model, thread_id = Threads._threadref_by_index(cr, uid, thread_index)
+                found = bool(model and thread_id)
+        return (res, model, thread_id) if found else None
+
+    @classmethod
+    def apply(cls, obj, cr, uid, routes, message, data=None, context=None):
+        def valid(route):
+            # A valid route is one that does not shadow our Reply-to and does
+            # not create a new thread (the classical fallback to crm.lead)
+            return route[0] != model and bool(route[1])
+        _, model, thread_id = data
+        # Must change the current `routes` **in-place**.
+        routes[:] = [route for _pos, route in cls.find_route(routes, valid)]
+        # TODO: Find the true user_id
+        routes.append((model, thread_id, {}, SUPERUSER_ID, None))
+        return routes
