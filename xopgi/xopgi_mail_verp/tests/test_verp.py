b4e2aa2ac72b61931bb553c13a32705fc64e8212
Manuel Vázquez Acosta <manu@merchise.org>
2015-06-26 15:58:20 -0400

    Add test skeleton.

--- xopgi/xopgi_mail_verp/tests/test_verp.py
+++ xopgi/xopgi_mail_verp/tests/test_verp.py
@@ -18,5 +18,17 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 
-import unittest2
-from openerp.addons.mail.tests.common import TestMail
+def test_ensure_bounce_address():
+    pass
+
+
+def test_route_bounce():
+    pass
+
+
+def test_ignore_forged_bounces_by_heuritics():
+    pass
+
+
+def test_ignore_forged_bounces_by_db():
+    pass
