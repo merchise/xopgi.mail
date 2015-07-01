c6cbf0a5b2c621e871f804392e70e6c69dc12728
Manuel Vázquez Acosta <manu@merchise.org>
2015-07-01 11:35:03 -0400

    Draft an email disclosure addon.

---
+++ xopgi/xopgi_mail_disclosure/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# __init__
+# ---------------------------------------------------------------------
+# Copyright (c) 2015 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2015-06-17
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+
+from . import disclose
