3b5fe8dc902006ad277658a5031626c47704ddca
Yoany <yoany.r@gmail.com>
2015-07-15 13:32:40 -0400

    Hide js buttons if user doesn't have access.

--- xopgi/xopgi_mail_move_message/__init__.py
+++ xopgi/xopgi_mail_move_message/__init__.py
@@ -1,1 +1,28 @@
 from . import mail_move_message_wizard # noqa
+from openerp import SUPERUSER_ID
+from openerp.osv import osv, fields
+
+
+FIELD_NAME = 'can_move'
+
+
+class Message(osv.Model):
+    _inherit = 'mail.message'
+
+    def _get_can_move(self, cr, uid, ids, field_name, args, context=None):
+        result = {}
+        has_group = self.pool['res.users'].has_group(
+            cr, uid, 'xopgi_mail_new_thread.group_new_thread')
+        for msg in self.browse(cr, uid, ids, context=context):
+            result[msg.id] = (msg.type != 'notification' and
+                              (uid == SUPERUSER_ID or has_group))
+        return result
+
+    _columns = {FIELD_NAME: fields.function(_get_can_move, string='Can New',
+                                            type='boolean'), }
+
+    def _message_read_dict(self, cr, uid, msg, parent_id=False, context=None):
+        res = super(Message, self)._message_read_dict(
+            cr, uid, msg, parent_id=parent_id, context=context)
+        res[FIELD_NAME] = msg.can_new_thread
+        return res
--- xopgi/xopgi_mail_new_thread/__init__.py
+++ xopgi/xopgi_mail_new_thread/__init__.py
@@ -1,1 +1,32 @@
 from . import mail_new_thread_wizard # noqa
+from openerp import SUPERUSER_ID
+from openerp.osv import osv, fields
+
+
+FIELD_NAME = 'can_new_thread'
+
+
+class Message(osv.Model):
+    _inherit = 'mail.message'
+
+    def _get_can_new(self, cr, uid, ids, field_name, args, context=None):
+        result = {}
+        has_group = self.pool['res.users'].has_group(
+            cr, uid, 'xopgi_mail_new_thread.group_new_thread')
+        for msg in self.browse(cr, uid, ids, context=context):
+            result[msg.id] = (msg.type != 'notification' and
+                              (uid == SUPERUSER_ID or has_group))
+        return result
+
+    _columns = {
+        FIELD_NAME:
+            fields.function(_get_can_new, string='Can New',
+                            type='boolean'),
+    }
+
+    def _message_read_dict(self, cr, uid, msg, parent_id=False,
+                           context=None):
+        res = super(Message, self)._message_read_dict(
+            cr, uid, msg, parent_id=parent_id, context=context)
+        res[FIELD_NAME] = msg.can_new_thread
+        return res
