d8ebeec6eaa87cd8d6f7c3bc2508072394d4690a
Manuel Vázquez Acosta <manu@merchise.org>
2015-10-12 19:18:24 -0400

    Integrate no-replying for forwarding messages.
    This addon integrates the xopgi_mail_nowall with xopgi_mail_forward.
    When both addons are installed this should installed automatically to
    avoid replies being sent via the "forward" button.

---
+++ xopgi/xopgi_mail_nowall_forward/__openerp__.py
@@ -0,0 +1,14 @@
+dict(
+    # Internal addon, it should be auto-installed when both no-wall and
+    # forward are installed.x
+    auto_install=True,
+    depends=['xopgi_mail_nowall', 'xopgi_mail_foward'],
+    name='xopgi_mail_nowall_forward',
+    summary='Makes the no-replying from wall be activated for forwarding',
+    description='Makes the no-replying from wall be activated for forwarding',
+    application=False,
+    installable=True,
+    data=[
+        'view/assets.xml',
+    ],
+)
