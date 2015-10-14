fd42dc896230437ba87fa34011e03632f326ef5e
Manuel Vázquez Acosta <manu@merchise.org>
2015-10-13 20:10:11 -0400

    Fix typos.

--- xopgi/xopgi_mail_nowall_forward/__openerp__.py
+++ xopgi/xopgi_mail_nowall_forward/__openerp__.py
@@ -2,13 +2,13 @@ dict(
     # Internal addon, it should be auto-installed when both no-wall and
     # forward are installed.
     auto_install=True,
-    depends=['xopgi_mail_nowall', 'xopgi_mail_foward'],
+    depends=['xopgi_mail_nowall', 'xopgi_mail_forward'],
     name='xopgi_mail_nowall_forward',
     summary='Makes the no-replying from wall be activated for forwarding',
     description='Makes the no-replying from wall be activated for forwarding',
     application=False,
     installable=True,
     data=[
-        'view/assets.xml',
+        'views/assets.xml',
     ],
 )
