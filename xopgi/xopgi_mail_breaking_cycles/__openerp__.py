82473dc45b2dde3f1f438b32d31e33a192a09937
carlos.abel <carlos.abel@merchise.org>
2015-11-10 21:13:06 -0500

    Breacking cycles when we send messages auto-submitted

---
+++ xopgi/xopgi_mail_breaking_cycles/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# xopgi.xopgi_mail_breaking_cycles.__openerp__
+# ---------------------------------------------------------------------
+# Copyright (c) 2014, 2015 Merchise Autrement
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+
+{
+    "name": "Breaking Cycles (xopgi)",
+    "version": "1.0",
+    "author": "Merchise Autrement",
+    "website": "http://xopgi.merchise.org/addons/xopgi_mail_breaking_cycles",
+    'category': 'Mail',
+    "description": ('Breaking Cycles sending messages.'),
+    "depends": ['xopgi_mail_threads', ],
+    "data": [],
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+}
