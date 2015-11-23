b1f41bb5f34d4ebbfe0794cff93cbfd7945fca32
Manuel Vázquez Acosta <manu@merchise.org>
2015-11-23 09:19:54 -0500

    Fix the breaking cycle router/transport.
    1. If the router matches the breaking-cycle address , nothing should be
       done with the message.  It was passing.
    
    2. There fixed values in the algorithm: an address
       'breaking-cycles+8jfh764@lahavane.com'.
    
       The 'breaking-cycle' prefix was being tested to begin with
       'breaking-cycle'.
    
    3. The transport used 'mail.catchall.alias' to build the address whereas
       the router asked for 'mail.replyto.alias'.  They would never pair!
    
    4. The transport might have applied even when the message was not an
       auto-submitted one.
    
    etc...

--- xopgi/xopgi_mail_breaking_cycles/router.py
+++ xopgi/xopgi_mail_breaking_cycles/router.py
@@ -15,14 +15,14 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-from openerp import SUPERUSER_ID
+from xoutil import logger as _logger
+
 from openerp.addons.xopgi_mail_threads import MailRouter, MailTransportRouter
 from openerp.addons.xopgi_mail_threads import TransportRouteData
 from openerp.addons.xopgi_mail_threads.utils import set_message_from
 from openerp.addons.xopgi_mail_threads.utils import set_message_sender
 
 
-DEFAULT_REPLYTO_PREFIX = 'catchall'
 #
 # This pair of Router & Transport avoid the mail-sending cycles when two
 # or more parties in a single thread have auto-responders:
@@ -46,49 +46,50 @@ DEFAULT_REPLYTO_PREFIX = 'catchall'
 #
 
 
+# The default alias for breaking cycle address.  You may configure the
+# parameter 'mail.breaking-cycle.alias'.
+DEFAULT_BREAKING_PREFIX = 'breaking-cycle'
+BREAKING_ALIAS_PARAMETER = 'mail.breaking-cycle.alias'
+
+
 class BreakingCyclesTransport(MailTransportRouter):
     @classmethod
     def query(self, obj, cr, uid, message, context=None):
         msg, _ = self.get_message_objects(obj, cr, uid, message,
                                           context=context)
-        address = self._get_replyto(obj, cr, uid, msg.thread_index,
-                                    context=context)
         if message['Auto-Submitted']:
-            # FIXME: Cortar el cable.
-            address = 'breaking-cycles+8jfh764@lahavane.com'
-            return bool(address), dict(thread_index=msg.thread_index,
-                                       replyto_address=address)
-        if msg and msg.thread_index:
-            return bool(address), dict(thread_index=msg.thread_index,
-                                       replyto_address=address)
+            address = self._get_breaking_address(
+                obj, cr, uid, msg.thread_index, context=context)
+            _logger.info('Re-sending auto-submitted message with a '
+                         'breaking-cycle address %s', address)
+            return bool(address), address
         return False, None
 
-    def prepare_message(self, obj, cr, uid, message, data=None, context=None):
-        address = data['replyto_address']
-        self._set_replyto(obj, cr, uid, message, address,
+    def prepare_message(self, obj, cr, uid, message, address=None,
+                        context=None):
+        assert address
+        self._set_headers(obj, cr, uid, message, address,
                           context=context)
-        del message['Return-Path']
-        message['Return-Path'] = address
         return TransportRouteData(message, {})
 
     @classmethod
-    def _set_replyto(cls, obj, cr, uid, message, address,
+    def _set_headers(cls, obj, cr, uid, message, address,
                      context=None):
         del message['Reply-To']
         message['Reply-To'] = address
+        del message['Return-Path']
+        message['Return-Path'] = address
         set_message_sender(message, address)
         set_message_from(message, address, address_only=True)
 
     @classmethod
-    def _get_replyto(cls, obj, cr, uid, thread_index,
-                     context=None):
+    def _get_breaking_address(cls, obj, cr, uid, thread_index, context=None):
         get_param = obj.pool['ir.config_parameter'].get_param
-        replyto = get_param(cr, uid, 'mail.catchall.alias',
-                            default=DEFAULT_REPLYTO_PREFIX,
-                            context=context)
+        alias = get_param(cr, uid, BREAKING_ALIAS_PARAMETER,
+                          default=DEFAULT_BREAKING_PREFIX, context=context)
         domain = get_param(cr, uid, 'mail.catchall.domain', context=context)
         if domain:
-            return '%s+%s@%s' % (replyto, thread_index, domain)
+            return '%s+%s@%s' % (alias, thread_index, domain)
         else:
             return None
 
@@ -103,42 +104,46 @@ class BreakingCyclesRouter(MailRouter):
             decode_header(message, 'Cc'),
             decode_header(message, 'Delivered-To')
         ]
-        recipients = [email for _, email in getaddresses(headers)]
-        match = cls._find_replyto(obj, cr, uid, recipients, context=context)
-        return match is not None, match
+        if message['Auto-Submitted']:
+            # If the message we're receiving is not an auto-submitted one,
+            # it's not a cycle.  This would be the case of:
+            #
+            # 1. Party A sends an auto-submitted message
+            # 2. Odoo resends that message to Party B.
+            # 3. Party B does not auto-respond, and that's just fine.
+            # 4. Later Party B replies but uses the address in the
+            #    auto-submitted message to do it.
+            #
+            # In this, although the address would be a breaking cycle is not
+            # actually a cycle.
+            #
+            # The only problem with this is there's a auto-responder that does
+            # not use the Auto-Submitted header.  As we've seen some use the
+            # From address as the recipient for the auto-submitted message and
+            # that's not what the RFC recommends.  However, the previous
+            # depiction is likely to happen is we ever allow an auto-submitted
+            # message to go out (as we do).
+            #
+            recipients = [
+                email for _, email in getaddresses(headers)
+                if cls._is_breaking_cycle_address(cls, obj, cr, uid, email)
+            ]
+            # If any recipient is a breaking cycle address, break it!
+            return bool(recipients)
+        else:
+            return False
 
     @classmethod
-    def _find_replyto(cls, obj, cr, uid, recipients, context=None):
-        from xoutil.string import cut_prefix, cut_suffix
+    def _is_breaking_cycle_address(cls, obj, cr, uid, address, context=None):
         get_param = obj.pool['ir.config_parameter'].get_param
-        domain = get_param(cr, uid, 'mail.catchall.domain', context=context)
-        if not domain:
-            return None
-        replyto = get_param(cr, uid, 'mail.replyto.alias',
-                            default=DEFAULT_REPLYTO_PREFIX, context=context)
-        prefix, suffix = replyto + '+', '@' + domain
-        found, model, thread_id = None, None, None
-        Threads = obj.pool['mail.thread']
-        while not found and recipients:
-            res = recipients.pop(0)
-            if prefix.startswith('breaking-cycles'):
-                model, thread_id = None, None
-            elif res.startswith(prefix):
-                thread_index = cut_prefix(cut_suffix(res, suffix), prefix)
-                model, thread_id = Threads._threadref_by_index(cr, uid,
-                                                               thread_index)
-                found = bool(model and thread_id)
-        return (res, model, thread_id) if found else None
+        prefix = get_param(cr, uid, BREAKING_ALIAS_PARAMETER,
+                           default=DEFAULT_BREAKING_PREFIX, context=context)
+        return address.startswith(prefix + '+')
 
     @classmethod
     def apply(cls, obj, cr, uid, routes, message, data=None, context=None):
-        def valid(route):
-            # A valid route is one that does not shadow our Reply-to and does
-            # not create a new thread (the classical fallback to crm.lead)
-            return route[0] != model and bool(route[1])
-        _, model, thread_id = data
-        # Must change the current `routes` **in-place**.
-        routes[:] = [route for _pos, route in cls.find_route(routes, valid)]
-        # TODO: Find the true user_id
-        routes.append((model, thread_id, {}, SUPERUSER_ID, None))
+        # This method is only called when the router match the breaking-cycles
+        # alias.  This means we detected a cycle, and NOTHING should be done
+        # with this message.
+        routes[:] = []
         return routes
