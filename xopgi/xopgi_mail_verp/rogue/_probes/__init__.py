8f4795611d07f7d6d5feb84d8d3bb67d93504ebc
Manuel Vázquez Acosta <manu@merchise.org>
2016-03-02 00:16:35 -0500

    Add the flufl probe.

--- xopgi/xopgi_mail_verp/rogue/_probes/__init__.py
+++ xopgi/xopgi_mail_verp/rogue/_probes/__init__.py
@@ -6,4 +6,14 @@ Probes must have a single ``__call__(self, message)``.  See the
 
 '''
 
+# The router use the probes in the order given by __all__, so you may prefer a
+# probe other another.
+__all__ = [
+    'NautaProbe',
+
+    # Keep this at the bottom
+    'FLUFLProbe',
+]
+
 from .nauta import NautaProbe
+from .heuristics import FLUFLProbe
