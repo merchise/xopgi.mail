b925fcdfd710f7c2e50718e58647478ced6013a8
Johanny <johanny@merchise.org>
2016-05-02 21:09:43 -0400

    Add xopgi_one_line_subject module.

---
+++ xopgi/xopgi_one_line_subject/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# --------------------------------------------------------------------------
+# xopgi_one_line_subject
+# --------------------------------------------------------------------------
+# Copyright (c) 2015-2016 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# Author: Merchise Autrement
+# Contributors: see CONTRIBUTORS and HISTORY file
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+
+from __future__ import absolute_import as _py3_abs_imports
+
+from openerp.release import version_info as ODOO_VERSION_INFO
+
+if ODOO_VERSION_INFO < (10, 0):
+    # MIGRATION POLICY: All addons are not included until someone work on them
+    # and upgrade them.
+
+    from . import transport  # noqa
