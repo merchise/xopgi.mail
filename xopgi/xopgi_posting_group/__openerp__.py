dfd9bbfd86a7fc215dce5f703e0e4a626ac63230
Abel Alfonso Fírvida Donéstevez <abel@merchise.org>
2016-07-05 09:57:03 -0400

    Adding views/mail_group_view.xml

--- xopgi/xopgi_posting_group/__openerp__.py
+++ xopgi/xopgi_posting_group/__openerp__.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "xopgi.posting_group",
-    'summary': "Allow to disable autofollowing on Odoo groups",
-    'description': "Allow to disable autofollowing in mail groups.",
+    'name': 'xopgi.posting_group',
+    'summary': 'Allow to disable autofollowing on Odoo groups',
+    'description': 'Allow to disable autofollowing in mail groups.',
 
-    "author": "Merchise Autrement",
-    "website": "http://xhg.ca.merchise.org/addons/xopgi_posting_group",
+    'author': 'Merchise Autrement',
+    'website': 'http://xhg.ca.merchise.org/addons/xopgi_posting_group',
     'category': 'Hidden',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['mail', ],
+
+    'data': ['views/mail_group_view.xml', ]
 }
