1bd9eef9fafd78afa38c9d906d7ba59de08c6c78
Abel Alfonso Fírvida Donéstevez <abel@merchise.org>
2016-07-05 13:27:38 -0400

    Working fine

--- xopgi/xopgi_posting_group/no_autofollow.py
+++ xopgi/xopgi_posting_group/no_autofollow.py
@@ -9,12 +9,11 @@
 # terms of the LICENCE attached (see LICENCE file) in the distribution
 # package.
 #
-# @created: 2016-06-18
+# @created: 2016-07-05
 
 from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
-
 from openerp import api, fields, models
 
 
@@ -27,9 +26,22 @@ class MailGroup(models.Model):
         help='If checked, this group will not auto subscribe mail senders'
     )
 
-    @api.model
-    def create(self, vals):
-        # INFO: Disable author auto following if asked
-        if self.enable_auto_subscribe:
-            self = self.with_context(mail_create_nosubscribe=True)
-        return super(MailGroup, self).create(vals)
+    @api.cr_uid_ids_context
+    def message_post(self, cr, uid, thread_id, body='', subject=None,
+                     type='notification', subtype=None, parent_id=False,
+                     attachments=None, context=None, content_subtype='html',
+                     **kwargs):
+
+        group = self.browse(cr, uid, thread_id, context=context)
+
+        if not group.enable_auto_subscribe:
+            if not context:
+                context = {}
+            context['mail_create_nosubscribe'] = True
+            context['mail_post_autofollow'] = False
+
+        return super(MailGroup, self).message_post(cr, uid, thread_id, body,
+                                                   subject, type, subtype,
+                                                   parent_id, attachments,
+                                                   context, content_subtype,
+                                                   **kwargs)
