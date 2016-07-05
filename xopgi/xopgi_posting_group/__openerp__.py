503996dd94229fd46001541297f663ef26fff759
Manuel Vázquez Acosta <manu@merchise.org>
2016-07-05 01:32:02 -0400

    Correct dependencies.

--- xopgi/xopgi_posting_group/__openerp__.py
+++ xopgi/xopgi_posting_group/__openerp__.py
@@ -1,26 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': "xopgi.posting_group",
-
-    'summary': """
-        Disable autofollowing on Odoo groups""",
-
-    'description': """
-        This module is intended to disable autofollowing for user that
-        send messages to Odoo groups """,
+    'summary': "Allow to disable autofollowing on Odoo groups",
+    'description': "Allow to disable autofollowing in mail groups.",
 
     "author": "Merchise Autrement",
     "website": "http://xhg.ca.merchise.org/addons/xopgi_posting_group",
-    'category': 'Mail',
+    'category': 'Hidden',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/security.xml',
-        # 'security/ir.model.access.csv',
-        # 'views/mail_group_view.xml'
-    ],
+    'depends': ['mail', ],
 }
