440afdff98a06591b624bdb53f89a909a1ed275d
Manuel Vázquez Acosta <manu@merchise.org>
2016-12-05 10:05:34 -0500

    Avoid warning about fast_suite being deprecated.

--- xopgi/xopgi_mail_verp/tests/__init__.py
+++ xopgi/xopgi_mail_verp/tests/__init__.py
@@ -1,5 +1,12 @@
+try:
+    from openerp.release import version_info as VERSION_INFO
+except ImportError:  # Odoo 10+
+    from odoo.release import version_info as VERSION_INFO
+
 from . import test_verp
 
-checks = [
-    test_verp,
-]
+
+if VERSION_INFO < (8, 0):
+    checks = [
+        test_verp,
+    ]
