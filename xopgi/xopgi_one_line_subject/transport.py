4c990324d0aa1ab5936267f24dadae2bebc0d13b
Manuel Vázquez Acosta <manu@merchise.org>
2017-01-14 03:16:22 -0500

    Upgrade xopgi_one_line_subject to new api.

--- xopgi/xopgi_one_line_subject/transport.py
+++ xopgi/xopgi_one_line_subject/transport.py
@@ -15,30 +15,31 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-from openerp.addons.xopgi_mail_threads import MailTransportRouter
-from openerp.addons.xopgi_mail_threads import TransportRouteData
+try:
+    from openerp.addons.xopgi_mail_threads import MailTransportRouter
+    from openerp.addons.xopgi_mail_threads import TransportRouteData
+except ImportError:
+    from odoo.addons.xopgi_mail_threads import MailTransportRouter
+    from odoo.addons.xopgi_mail_threads import TransportRouteData
 
 try:
     # Odoo 8
     from openerp.addons.mail.mail_message import decode
 except ImportError:
     # Odoo 9 fallback
-    from openerp.addons.mail.models.mail_message import decode
+    try:
+        from openerp.addons.mail.models.mail_message import decode
+    except ImportError:
+        from odoo.addons.mail.models.mail_message import decode
 
 
 class OneLineSubjectTransport(MailTransportRouter):
-    '''A MailTransport for ensure subject with only one line.
-
-    '''
-
+    'A MailTransport for ensure subject with only one line.'
     @classmethod
     def query(self, *args, **kwargs):
-        ''' Always apply.
-
-        '''
         return True, None
 
-    def prepare_message(self, obj, cr, uid, message, data=None, context=None):
+    def prepare_message(self, obj, message, data=None):
         '''Remove new line character from message subject.
 
         '''
