4f2efd074d7645a2beff2fb4fe1109383dd634a2
Manuel Vázquez Acosta <manu@merchise.org>
2017-01-15 10:26:31 -0500

    Update xopgi_mail_expose_recipients to new api.

--- xopgi/xopgi_mail_expose_recipients/__init__.py
+++ xopgi/xopgi_mail_expose_recipients/__init__.py
@@ -16,4 +16,14 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-from . import models  # noqa
+try:
+    from openerp.release import version_info as ODOO_VERSION_INFO
+except ImportError:
+    from odoo.release import version_info as ODOO_VERSION_INFO
+
+
+if ODOO_VERSION_INFO < (9, 0):
+    # MIGRATION POLICY: All addons are not included until someone work on them
+    # and upgrade them.
+
+    from . import models  # noqa
--- xopgi/xopgi_mail_expose_recipients/models.py
+++ xopgi/xopgi_mail_expose_recipients/models.py
@@ -16,8 +16,13 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-from openerp import api, fields, models
-from openerp.tools.mail import email_split_and_format
+try:
+    from openerp import api, fields, models
+    from openerp.tools.mail import email_split_and_format
+except ImportError:
+    from odoo import api, fields, models
+    from odoo.tools.mail import email_split_and_format
+
 
 from xoeuf.osv.orm import get_modelname
 
@@ -26,9 +31,14 @@ try:
     from openerp.addons.mail.mail_thread import mail_thread, decode_header
     from openerp.addons.mail.mail_message import mail_message
 except ImportError:
-    # Odoo 9 fallback
-    from openerp.addons.mail.models.mail_thread import mail_thread, decode_header
-    from openerp.addons.mail.models.mail_message import mail_message
+    try:
+        # Odoo 9 fallback
+        from openerp.addons.mail.models.mail_thread import mail_thread, decode_header
+        from openerp.addons.mail.models.mail_message import mail_message
+    except ImportError:
+        # Odoo 10
+        from odoo.addons.mail.models.mail_thread import mail_thread, decode_header
+        from odoo.addons.mail.models.mail_message import mail_message
 
 
 class MailMessage(models.Model):
