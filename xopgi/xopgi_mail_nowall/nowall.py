350e5cef59ca7dfcd9554d23fe9ab140aacc4279
Oraldo Jacinto Simón <oraldo@merchise.org>
2017-03-15 16:12:08 -0400

    Make xopgi_mail_nowall work in Odoo 9 and 10.

--- xopgi/xopgi_mail_nowall/nowall.py
+++ xopgi/xopgi_mail_nowall/nowall.py
@@ -24,9 +24,9 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 try:
-    from openerp.addons.xopgi_mail_threads.routers import MailRouter
-except ImportError:
     from odoo.addons.xopgi_mail_threads.routers import MailRouter
+except ImportError:
+    from openerp.addons.xopgi_mail_threads.routers import MailRouter
 
 
 MODEL_INDEX = 0
