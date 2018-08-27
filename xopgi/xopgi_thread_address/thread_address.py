44755cfb2b6d1256fdb235d728b546b75f1c48a1
Manuel Vázquez Acosta <manuel@merchise.org>
2018-08-27 11:52:34 -0400

    Adapt to latest changes in xopgi_mail_threads.

--- xopgi/xopgi_thread_address/thread_address.py
+++ xopgi/xopgi_thread_address/thread_address.py
@@ -157,5 +157,5 @@ class UniqueAddressRouter(MailRouter):
         # Must change the current `routes` **in-place**.
         routes[:] = [route for _pos, route in cls.find_route(routes, valid)]
         # TODO: Find the true user_id
-        routes.append((model, thread_id, dict(message=message), SUPERUSER_ID, None))
+        routes.append((model, thread_id, dict(original_message=message), SUPERUSER_ID, None))
         return routes
