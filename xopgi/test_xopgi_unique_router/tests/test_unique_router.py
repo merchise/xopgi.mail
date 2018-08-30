096e576708220bee82fa3be5e64b3edb01847ca7
larisa <larisa@merchise.org>
2018-08-30 19:39:31 -0400

    Recoding test for checking bounce sent.
    Checking if bounce is sent and BOUNCE ROUTE MODEL have the expected
    record. This way it can be said that the route bounce is taken.

--- xopgi/test_xopgi_unique_router/tests/test_unique_router.py
+++ xopgi/test_xopgi_unique_router/tests/test_unique_router.py
@@ -11,6 +11,9 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 from xoeuf.odoo.tests.common import TransactionCase
+from xoeuf import api
+import email
+from xoeuf.odoo.addons.xopgi_mail_threads.stdroutes import BOUNCE_ROUTE_MODEL
 
 NEW_MESSAGE = '''\
 From: = User <user@gmail.com>
@@ -52,9 +55,42 @@ class TestMailRouter(TransactionCase):
             NEW_MESSAGE
         )
         self.thread = self.env['test_xopgi_unique_router.model'].browse(obj_id)
+        self.bounce_sent = False
+
+        def set_bounce_sent(value):
+            self.bounce_sent = value
+
+        @api.model
+        def _check_bounce_sent(self, message, *args, **kwargs):
+            MailThread = self.env['mail.thread']
+            msg_txt = email.message_from_string(REPLY_INVALID_ADDRESS)
+            msg = MailThread.message_parse(msg_txt)
+            if message['from'].startswith('MAILER-DAEMON') and message[
+                'to'] == msg.get('from') and message['subject'].startswith(
+                    'Re:') and message['subject'].endswith(msg.get('subject')):
+                set_bounce_sent(True)
+            return _check_bounce_sent.origin(self, message, *args, **kwargs)
+
+        self._unpatch_send_email()
+        self.env['ir.mail_server']._patch_method(
+            'send_email',
+            _check_bounce_sent
+        )
 
     def tearDown(self):
         super(TestMailRouter, self).tearDown()
+
+    def _unpatch_send_email(self):
+        # The pair of methods _patch_method and _revert_method are
+        # ill-defined.  In this case: the tests of the addon 'mail' patch the
+        # ir.mail_server, and revert, this causes the Model Class to be
+        # modified with by setting the 'origin' code of in the Model Class.
+        # The Model Class should be empty: so we simply remove the send_email
+        # method and allow the MRO to execute smoothly.
+        try:
+            del type(self.env['ir.mail_server']).mro()[0].send_email
+        except:  # noqa
+            pass
 
     def test_unique_address_router(self):
         reply = REPLY.format(thread_index=self.thread.thread_index)
@@ -71,5 +107,5 @@ class TestMailRouter(TransactionCase):
             'test_xopgi_unique_router.model',
             REPLY_INVALID_ADDRESS
         )
-        # TODO: Test that the bounce route is being taken.
-        self.failIf(thread_id)
+        thread_found = self.env[BOUNCE_ROUTE_MODEL].browse(thread_id)
+        self.assert_(self.bounce_sent and thread_found)
