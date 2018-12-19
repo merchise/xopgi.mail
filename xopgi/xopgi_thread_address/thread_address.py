08fb5d128c852638d3dc43ce4a5d5cdceb09e4d3
Manuel Vázquez Acosta <manuel@merchise.org>
2018-12-19 16:28:27 -0500

    Instead of sending bounces, log a warning.

--- xopgi/xopgi_thread_address/thread_address.py
+++ xopgi/xopgi_thread_address/thread_address.py
@@ -132,22 +132,28 @@ class UniqueAddressRouter(MailRouter):
         replymarks = get_param('mail.replyto.alias',
                                default=DEFAULT_REPLYTO_PREFIX)
         prefixes = [alias.strip() + '+' for alias in replymarks.split(',')]
-        found, model, thread_id = None, None, None
         Threads = obj.env['mail.thread']
-        matches = None
+        found, model, thread_id, match = None, None, None, None
         while not found and recipients:
             res = recipients.pop(0)
             if any(res.startswith(p) and res.endswith(domain) for p in prefixes):
-                matches = res
+                match = res
                 thread_index = res[res.find('+') + 1:res.find('@')]
                 model, thread_id = Threads._threadref_by_index(thread_index)
                 found = bool(model and thread_id)
-        if not found and matches:
+        if not found and match:
             # It matches a unique address format but thread not found.  Lets
             # provide a route that allow to process the message properly.
             # Also sender will be notified that the address given is no longer
             # valid.
-            return (BOUNCE_ROUTE_MODEL, None)
+            _logger.warning(
+                'I would have sent a bounce here!',
+                extra=dict(
+                    match=match,
+                    stack=True,
+                )
+            )
+            return None  # (BOUNCE_ROUTE_MODEL, None)
         return (model, thread_id) if found else None
 
     @classmethod
